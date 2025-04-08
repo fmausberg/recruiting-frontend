1108e58c6f3149b62b33d2d3dd568fd2380b25ac
fmausberg <felix.mausberg@gmail.com>
2025-04-08 15:50:27 +0200

    switched user page path

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useState, useEffect } from "react";
-import { useAuth } from "../../../context/AuthContext";
+import { useAuth } from "../../context/AuthContext";
 
 const fetchWithToken = async (url: string, options: RequestInit = {}) => {
   const token = getCookie("jwttoken");
